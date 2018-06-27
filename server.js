92c77bf05cb6b76d9f0de5b0972f7e700d9dc86c
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-06-27 16:42:15 -0500

    cleaned up integration test

--- server.js
+++ server.js
@@ -1,37 +1,15 @@
-const express = require('express');
+"use strict";
+
+const express = require("express");
+
 const app = express();
 
-app.use(express.static('public'));
-
-let server;
-
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
-    });
-  });
-}
-
-function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
-  });
-}
+app.use(express.static("public"));
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+  app.listen(process.env.PORT || 8080, function() {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
 
-module.exports = {app, runServer, closeServer};
+module.exports = app;
--- test/test-server.js
+++ test/test-server.js
@@ -1,24 +1,21 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const {app, runServer, closeServer} = require('../server');
+"use strict";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server.js");
+
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Root', function() {
-  before(function() {
-    return runServer();
-  });
-
-  after(function() {
-    return closeServer();
-  });
-
-  it('should return status code 200 and HTML on GET', function() {
-    return chai.request(app)
-      .get('/')
+describe("index page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get("/")
       .then(function(res) {
-        chai.expect(res).to.have.status(200);
-        chai.expect(res).to.be.html;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
       });
   });
 });
